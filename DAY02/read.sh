1751f0754f939fddc0eb201f4ca0ee07280c6438
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-09 06:59:34 +0000

    updated

--- DAY02/read.sh
+++ DAY02/read.sh
@@ -3,3 +3,6 @@
 echo "Please enter the username"
 read USERNAME #take input from user
 echo "username entered is: $USERNAME"
+echo "Please enter the password"
+read PASSWORD #take input from user
+echo "password entered is: $PASSWORD"
