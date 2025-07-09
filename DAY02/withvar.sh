b30019c8096542950daa3bdbbaa0ca7c986dbeba
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-09 06:28:37 +0000

    updated

--- DAY02/withvar.sh
+++ DAY02/withvar.sh
@@ -2,7 +2,7 @@
 Person1=Sumati
 Person2=Sam
 
-echo "Person1: Hello Person2, Good Morning!!"
-echo "Person2: Hey Person1, Good Morning!!"
-echo "Person1: How are you doing Person2?"
-echo "Person2: Iam Good"
+echo "$Person1: Hello $Person2, Good Morning!!"
+echo "$Person2: Hey $Person1, Good Morning!!"
+echo "$Person1: How are you doing $Person2?"
+echo "$Person2: Iam Good"
