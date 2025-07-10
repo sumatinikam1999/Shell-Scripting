5acd9f164b3db9b799cfb3c7f677d6bad1aaeeaf
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-10 11:00:38 +0000

    updated

--- DAY03/installpkgusingloop.sh
+++ DAY03/installpkgusingloop.sh
@@ -5,3 +5,11 @@ for i in $@
 do
  yum install $i -y
 done
+
+#improvemnts
+#implement log file
+#implement colors
+#implement validations
+#implement validations through functions
+#your script should check package is already installed or not, if already installed print yellow color
+#If installed just print package is already installed it should not go for installation again
