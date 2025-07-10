84c5f2c55bff71fa0a206a76b59fa9f9769fcb27
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-10 08:50:24 +0000

    updated

--- DAY03/installmysql.sh
+++ DAY03/installmysql.sh
@@ -5,7 +5,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
     then
        echo "ERROR: Please run script with roo user"
-       exit 1
+       exit 1 #shell script won't understand by default to stop after seeing error so we have give exit status other than zero to exit if having error
 fi
 
 yum install git -y
+
+if [ $? -ne 0 ]
+    then
+       echo "Installation Failure"
+       exit 1
+else
+       echo "Installation is success"
+fi
