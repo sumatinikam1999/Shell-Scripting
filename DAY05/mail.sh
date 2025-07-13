7945cc99aa1860dbc496cda83f1ff2798c3442f8
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-13 14:11:23 +0000

    updated

--- DAY05/mail.sh
+++ DAY05/mail.sh
@@ -4,7 +4,9 @@
 # mail.sh TO_ADDRESS SUBJECT BODY TEAM_NAME ALERT_TYPE
 TO_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+#it will escape / 
+BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEMA_NAME=$4
 ALERT_TYPE=$5
 
