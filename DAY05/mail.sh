55d1824f07ded378893a9e8e53805545b639c98c
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-13 14:20:02 +0000

    updated

--- DAY05/mail.sh
+++ DAY05/mail.sh
@@ -13,5 +13,5 @@ ALERT_TYPE=$5
 #echo "all args: $@"
 
 # $ will work in double quote not single quote
-FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY" template.html)
-echo "$FINAL_BODY" | mailx -s "$SUBJECT" $TO_ADDRESS 
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
+echo "$FINAL_BODY" | mailx -s "$(echo "$SUBJECT\nContent-Type: text/html")" $TO_ADDRESS 
