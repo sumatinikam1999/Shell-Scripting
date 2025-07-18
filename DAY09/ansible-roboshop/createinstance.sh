e77c7582c154e9e34a4605316a4a6bf7d5e6fef6
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-07-18 13:50:15 +0000

    updated ami

--- DAY09/ansible-roboshop/createinstance.sh
+++ DAY09/ansible-roboshop/createinstance.sh
@@ -5,8 +5,8 @@ INSTANCE_TYPE=""
 SECURITY_GROUP_ID=sg-02b7456262aa1e272
 DOMAIN_NAME=devopslearner.space
 HOSTED_ZONE_ID=Z00027373O2OKHY987PPU
-AMI_LINUX2=ami-0c55b159cbfafe1f0
-AMI_LINUX2023=ami-0c101f26f147fa7fd
+AMI_LINUX2=ami-04890fefece4fb64f
+AMI_LINUX2023=ami-0a1235697f4afa8a4
 #if mysql or mongodb instance_type should be t3.medium, for all others is is t2.micro
 
 for i in $@
